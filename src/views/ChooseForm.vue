0a9d7c8f05da3dc904b79771163e3be51aeedd5c
Seitek2002 <seitekbegaliev@gmail.com>
2025-07-01 19:01:36 +0600

    qweqwe

--- src/views/ChooseForm.vue
+++ src/views/ChooseForm.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center gap-[10px] px-4">
+    <h2 class="font-bold text-center text-[28px]">
+      Онлайн оформление ОСАГО
+    </h2>
+    <img class="max-w-[300px]" src="../assets/images/logo.png" alt="">
+    <p class="text-center">
+      ЗАО «Бакай Иншуренс» является дочерней компанией ОАО «Бакай Банк», что является гарантом надежности и финансовой устойчивости бренда.
+    </p>
     <div class="font-bold text-center text-[20px]">Сколько водителей будет управлять автомобилем?</div>
     <router-link class="button" to="/documents-form"> Один водитель </router-link>
     <router-link class="button" to="/documents-form-2"> Несколько водителей </router-link>
