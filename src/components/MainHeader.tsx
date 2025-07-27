fd0b0e0eee7a4f2559cb7b6998df0592ae2828a8
Seitek2002 <seitekbegaliev@gmail.com>
2025-07-27 14:37:37 +0600

    changes

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,8 +1,8 @@
-import { useTranslation } from "react-i18next";
-import Container from "./Container";
-import type { FC } from "react";
-import { Link, useNavigate } from "react-router";
-import logo from "../assets/logo.svg";
+import { useTranslation } from 'react-i18next';
+import Container from './Container';
+import type { FC } from 'react';
+import { Link, useNavigate } from 'react-router';
+import logo from '../assets/logo.svg';
 
 interface IProps {
   type?: string;
@@ -13,48 +13,46 @@ const Header: FC<IProps> = ({ type }) => {
   const navigate = useNavigate();
 
   const handleLangSwitch = () => {
-    i18n.changeLanguage(i18n.language === "kg" ? "ru" : "kg");
+    i18n.changeLanguage(i18n.language === 'kg' ? 'ru' : 'kg');
   };
 
   return (
     <header
       className={` bg-[#fff] ${
-        type === "services"
-          ? "text-[#191D23] bg-white fixed top-0 left-0 w-full py-[16px] z-20"
-          : "fixed top-0 left-0 w-full py-[16px] z-20 text-[#fff]"
+        type === 'services'
+          ? 'text-[#191D23] bg-white fixed top-0 left-0 w-full py-[16px] z-20'
+          : 'fixed top-0 left-0 w-full py-[16px] z-20 text-[#fff]'
       }`}
     >
       <Container>
-        <div className="flex justify-between items-center">
+        <div className='flex justify-between items-center'>
           <img
             src={logo}
-            alt="Main Logo"
-            className="md:w-[150px] cursor-pointer"
-            onClick={() => navigate("/")}
+            alt='Main Logo'
+            className='md:w-[150px] cursor-pointer'
+            onClick={() => navigate('/')}
           />
-          <div className="flex items-center">
+          <div className='flex items-center'>
+            <Link to='/choose-form'>
+              <button
+                className='hidden md:block bg-[#1AA37F] py-[8px] px-[14px] rounded-[8px] cursor-pointer'
+                onClick={() => navigate('/')}
+              >
+                Начать зарабатывать
+              </button>
+            </Link>
             <span
-              className="lang-switch"
+              className='lang-switch'
               style={{
-                color: "#000",
-                cursor: "pointer",
-                background: "white",
-                padding: "0 10px",
+                color: '#000',
+                cursor: 'pointer',
+                background: 'white',
+                padding: '0 10px',
               }}
               onClick={handleLangSwitch}
             >
-              {i18n.language === "kg" ? "Русский" : "Кыргызча"}
+              {i18n.language === 'kg' ? 'Русский' : 'Кыргызча'}
             </span>
-            <Link
-              to="/choose-form"
-            >
-            <button
-              className="hidden md:block bg-[#1AA37F] py-[8px] px-[14px] rounded-[8px] cursor-pointer"
-              onClick={() => navigate("/")}
-            >
-              Начать зарабатывать
-            </button>
-            </Link>
           </div>
         </div>
       </Container>
